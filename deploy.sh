646528caf2183e394089b2b646ec9644123048b3
itkq <kuogsi@gmail.com>
2016-12-13 21:44:38 +0900

    :construction: Sudo again

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-curl -o /usr/local/bin/ecs-cli https://s3.amazonaws.com/amazon-ecs-cli/ecs-cli-linux-amd64-latest
-chmod +x /usr/local/bin/ecs-cli
+sudo curl -o /usr/local/bin/ecs-cli https://s3.amazonaws.com/amazon-ecs-cli/ecs-cli-linux-amd64-latest
+sudo chmod +x /usr/local/bin/ecs-cli
 
 ecs-cli configure --cluster Cookgram
 ecs-cli compose service up
