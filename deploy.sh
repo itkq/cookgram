e39f65d4c25bbfb9dd3f1f622fa9d4d98a3126fa
itkq <kuogsi@gmail.com>
2016-12-14 03:01:33 +0900

    :green_heart: Fix deploy config

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-sudo curl -o /usr/local/bin/ecs-cli https://s3.amazonaws.com/amazon-ecs-cli/ecs-cli-linux-amd64-latest
-sudo chmod +x /usr/local/bin/ecs-cli
+which ecs-cli
+if [ $? -ne 0 ] ; then
+  sudo curl -o /usr/local/bin/ecs-cli https://s3.amazonaws.com/amazon-ecs-cli/ecs-cli-linux-amd64-latest
+  sudo chmod +x /usr/local/bin/ecs-cli
+fi
 
 ecs-cli configure --cluster Cookgram
-ecs-cli compose service up
+ecs-cli compose up
